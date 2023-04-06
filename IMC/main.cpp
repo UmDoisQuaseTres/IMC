fcc76209c3e594722258ba86d4317bf3980c60a2
Matheus de Oliveira <um.dois.quase.tres@gmail.com>
2023-04-05 21:39:48 -0300

    Aplicado padrão regra dos 5, reduzindo o consumo de memoria em 97,33%

--- IMC/main.cpp
+++ IMC/main.cpp
@@ -8,15 +8,32 @@ private:
 	float *width = nullptr, *height = nullptr, *result = nullptr;
 
 public:
+	//Construtor alocando espaço na memoria para os ponteiros declarados acima
 	Imc() {
 		width = new float;
 		height = new float;
 		result = new float;
 	}
-	~Imc(){
-		delete width;
-		delete height;
-		delete result;
+	//Construtor alocando espaço na memoria para os ponteiros por parametros
+	Imc(float w, float h) { 
+		width = new float(w);
+		height = new float(h);
+		result = new float;
+	}
+	//Construtor cópia
+	Imc(const Imc& other) {
+		width = new float(*(other.width));
+		height = new float(*(other.height));
+		result = new float(*(other.result));
+	}
+	//Definindo operador de atribuição
+	Imc& operator=(const Imc& other) {
+		if (this != &other) {
+			*width = *(other.width);
+			*height = *(other.height);
+			*result = *(other.result);
+		}
+		return *this;
 	}
 
 	void setW(float w) {
@@ -38,15 +55,20 @@ public:
 int main() {
 	while (true) {
 
+		//Inicialização do Regex
 		std::regex digit_regex("^[0-9.]+$");
 
 		//Criação dos ponteiros
-		std::string *input_w = nullptr, *input_h = nullptr;
-		float *heightconv = nullptr, *widthconv = nullptr;
+		std::string* input_w = nullptr;
+		std::string *input_h = nullptr;
+		float* heightconv = nullptr;
+		float *widthconv = nullptr;
 
 		//Alocando memoria dinamica para os ponteiros
-		input_w = new std::string, input_h = new std::string;
-		heightconv = new float, widthconv = new float;
+		input_w = new std::string;
+		input_h = new std::string;
+		heightconv = new float;
+		widthconv = new float;
 
 		Imc calc{};
 
@@ -54,6 +76,7 @@ int main() {
 		while (true) {
 			std::cout << "Digite seu peso em kilos por favor: \n";
 			std::getline(std::cin, *input_h);
+
 			if (std::regex_match(*input_h, digit_regex)) {
 				*heightconv = std::stof(*input_h);
 				calc.SetH(*heightconv);
